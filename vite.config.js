7590e617c43059b845788e3ba7d710bd5ffe97ff
Tejsvi78 <tejsvisharma2@gmail.com>
2025-08-07 19:08:23 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { qrcode } from 'vite-plugin-qrcode'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [tailwindcss(), react(), qrcode()],
-  // base: "/AuraFlow",
+  base: "/AuraFlow",
   server: {
     host: true,
     port: 5173
